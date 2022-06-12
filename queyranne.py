e2379aabcde928b699bd28c43f0add75e19dad68
juandiegou <juandiegougallego@hotmail.com>
2022-06-12 23:53:05 +0000

    pendentpair

--- queyranne.py
+++ queyranne.py
@@ -1,87 +1,143 @@
-""""
-clase python que permite realiza el algoritmo de Queyranne
-en el cual se relaizan las k-particiones de un conjunto de datos
 """
+minimization of submodular functions
+to start:
+minimization of symmetric submodular set functions using Queyranne's algorithm
+Maurice Queyranne. 1998. Minimizing symmetric submodular functions. Mathematical
+Programming.
+Jason Manley, 2018
+"""
+
 import numpy as np
-import random
-import itertools as iter
-import math
+
+def pendent_pair(Vprime, V, S, f, params=None):
+    """ return Queyranne's pendent pair for function f over finite set V,
+        where the first (arbitary) input element x is V[0] """
+    x = 0
+    vnew = Vprime[x]
+    n = len(Vprime)
+    Wi = []
+    used = np.zeros((n,1))
+    used[x] = 1
+
+    for i in range(n-1):
+        vold = vnew
+        Wi = Wi + S[vold]
+
+        ## update keys
+        keys = np.ones((n,1))*np.inf
+        for j in range(n):
+            if used[j]:
+                continue
+            keys[j] = f(Wi + S[Vprime[j]], V, params) - f(S[Vprime[j]], V, params)
+
+        ## extract min
+        argmin = np.argmin(keys)
+        vnew = Vprime[argmin]
+        used[argmin] = 1
+        fval = np.min(keys)
+
+    s = vold
+    t = vnew
+
+    return s, t, fval
 
 
-class Queyranne:
-    def __init__(self, data, k):
-        self.data = data
-        self.k = k
-        self.partitions = []
-        self.centroids = []
-        self.clusters = []
-        self.cluster_size = []
-        self.cluster_mean = []
-        self.cluster_std = []
-        self.cluster_min = []
-        self.cluster_max = []
-        self.cluster_median = []
-        self.cluster_mode = []
-        self.cluster_var = []
-        self.cluster_iqr = []
-        self.cluster_skew = []
-        self.cluster_kurtosis = []
-        self.cluster_entropy = []
-        self.cluster_entropy_norm = []
-        self.cluster_entropy_norm_log = []
-        self.cluster_entropy_norm_log2 = []
-        self.cluster_entropy_norm_log10 = []
-        self.cluster_entropy_norm_log100 = []
-        self.cluster_entropy_norm_log1000 = []
-        self.cluster_entropy_norm_log10000 = []
-        self.cluster_entropy_norm_log100000 = []
-        self.cluster_entropy_norm_log1000000 = []
-        self.cluster_entropy_norm_log10000000 = []
-        self.cluster_entropy_norm_log100000000 = []
-        self.cluster_entropy_norm_log1000000000 = []
-        self.cluster_entropy_norm_log10000000000 = []
-        self.cluster_entropy_norm_log100000000000 = []
-        self.cluster_entropy_norm_log1000000000000 = []
-        self.cluster_entropy_norm_log10000000000000 = []
-        self.cluster_entropy_norm_log100000000000000 = []
-        self.cluster_entropy_norm_log1000000000000000 = []
-        self.cluster_entropy_norm_log10000000000000000 = []
+def diff(A, B):
+    """ find set difference (relative complement, or A\B) """
+    m = np.amax(np.array([np.amax(A), np.amax(B)]))
+    vals = np.zeros((m+1,1))
+    vals[A] = 1
+    vals[B] = 0
+    idx = np.nonzero(vals)
+    return idx[0]
 
-    def run(self):
-      """
-      metodo que permite ejecutar el algoritmo de Queyranne
-      """
-      self.partitions = self.k_partitions()
 
-    def k_partitions(self):
-      """
-      metodo que permite realizar las k-particiones de un conjunto de datos
-      """
-      partitions = []
-      for i in range(self.k):
-        partitions.append(self.data[i::self.k])
-        print(partitions)
-      return partitions
+def optimal_set(V, f, params=None):
+    """ implement Queyranne's algorithm for finding the minimum of symmetric
+        submodular function f over the finite set V
+        to utilize: define a function f(S, V, params). Queyranne's algorithm will
+        minimize f[S] + f[V\S] given any necessary parameters in params """
 
-    def permutations(self):
-      """
-      metodo que permite realizar las permutaciones de un conjunto de datos
-      """
-      permutations = iter.permutations(self.data)
-      
-      return [permutations]
+    n = len(V)
+    S = [[] for _ in range(n)]
+    for i in range(n):
+        S[i] = [V[i]]
+
+    p = np.zeros((n-1,1))
+    A = []
+    idxs = range(n)
+    for i in range(n-1):
+        ## find a pendant pair
+        t, u, fval = pendent_pair(idxs, V, S, f, params)
+
+        ## candidate solution
+        A.append(S[u])
+        p[i] = f(S[u],V,params)
+        S[t] = [*S[t], *S[u]]
+        idxs = diff(idxs, u)
+        S[u] = []
+
+    ## return minimum solution
+    i = np.argmin(p)
+    R = A[i]
+    fval = p[i]
+
+    ## make R look pretty
+    notR = diff(V,R)
+    R = sorted(R)
+    notR = sorted(notR)
+
+    if R[0] < notR[0]:
+        R = (tuple(R),tuple(notR))
+    else:
+        R = (tuple(notR),tuple(R))
+
+    return R, fval
+
+
+def k_subset(s, k):
+    if k == len(s):
+        return (tuple([(x,) for x in s]),)
+    k_subs = []
+    for i in range(len(s)):
+        partials = k_subset(s[0:i] + s[i + 1:len(s)], k)
+        for partial in partials:
+            for p in range(len(partial)):
+                k_subs.append(partial[:p] + (partial[p] + (s[i],),) + partial[p + 1:])
+    return k_subs
+
+
+def uniq_subsets(s):
+    u = set()
+    for x in s:
+        t = []
+        for y in x:
+            y = list(y)
+            y.sort()
+            t.append(tuple(y))
+        t.sort()
+        u.add(tuple(t))
+    return u
+
+
+def find_partitions(V,k):
+    """ find all partitions of elements in V that contain k members """
+    k_subs = k_subset(V,k)
+    k_subs = uniq_subsets(k_subs)
+
+    return k_subs
+
+
+def intersection(a, b):
+    """ return the intersection of two lists """
+    return list(set(a) & set(b))
+
+
+def union(a, b):
+    """ return the union of two lists """
+    return list(set(a) | set(b))
+
+
 
 if __name__ == '__main__':
-  """
-  metodo principal que permite ejecutar el algoritmo de Queyranne
-  """
-  data = np.array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
-  k = 3
-  queyranne = Queyranne(data, k)
-  queyranne.run()
-  print(queyranne.partitions)
-  """"
-  funcion lambda de permutaciones
-  """
-  permutations = list(map(lambda x: list(x), queyranne.permutations()))
-  print(permutations)
+  pendent_pair([1,2,3,4,5], [1,2,3,4,5], [[1,2,3],[4,5]], lambda x,y,z: sum(x) + sum(y) + z)
